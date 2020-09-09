08e421d225a9c2b89083f622d3abbf627c4fe90c
Saddam <khansaddamhussain22016@gmail.com>
2020-09-09 07:29:53 +0530

    main.py updated

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ with open('config.json','r')  as c:
 local_server = True
 
 app = Flask(__name__)
+
+
 if(local_server):
     app.config['SQLALCHEMY_DATABASE_URI'] = parameter['local_uri']
 else:
@@ -29,19 +31,32 @@ class Contacts(db.Model):
     massage = db.Column(db.String(200),  nullable=False)
     date = db.Column(db.String(12),  nullable=True)
 
+class Posts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80),  nullable=False)
+    tagline = db.Column(db.String(120), nullable=False)
+    slug = db.Column(db.String(30), nullable=False)
+    content = db.Column(db.String(500),  nullable=False)
+    img_file = db.Column(db.String(12), nullable=True)
+    date = db.Column(db.String(10),  nullable=True)
 
 
 @app.route("/")
 def home():
-    return render_template('index.html', parameter=parameter)
+    posts=Posts.query.filter_by().all()[0:5]
+    return render_template('index.html', parameter=parameter, posts=posts)
 
 @app.route("/about")
 def about():
     return render_template('about.html', parameter=parameter)
 
-@app.route("/post")
-def post():
-    return render_template('post.html', parameter=parameter)
+@app.route("/post/<string:post_slug>", methods=['GET'])
+def post_route(post_slug):
+    post=Posts.query.filter_by(slug=post_slug).first()
+
+
+    return render_template('post.html', parameter=parameter, post=post)
+
 
 @app.route("/contact",methods=['GET','POST'])
 def contact():
@@ -54,6 +69,7 @@ def contact():
         entry = Contacts(name=name,email=emailAdd,phone_number=phone,massage=message,date=datetime.now())
         db.session.add(entry)
         db.session.commit()
+
     return render_template('contact.html', parameter=parameter)
 
 app.run(debug=True)
